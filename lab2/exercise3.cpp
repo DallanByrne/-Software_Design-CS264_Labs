1fa3cd3d93d451f3e1f3ce811cc54f0a149aa5c5
Dallan Byrne <simplisticdude@gmail.com>
2017-10-10 09:48:54 +0100

    Update exercise3.cpp

--- lab2/exercise3.cpp
+++ lab2/exercise3.cpp
@@ -13,7 +13,12 @@ int main(){
 	cout << "Please enter an integer value: ";
 	cin >> n;
 
-	int arr[10];
+	int arr[11];
+	
+	for(int i =0; i< 11; i++ ){
+
+		arr[i]=0;	//Fills the array with 0s
+	}
 	
 	int sum, total;
 	int roll1, roll2;
@@ -21,22 +26,23 @@ int main(){
 	for(int i = 0;i < n; i++){
 		
 		
-		roll1 = rand() % 6 + 1;
+		roll1 = rand() % 6 + 1;		//First dice roll
 		cout<< "You rolled a "<<roll1<<" and ";
-		roll2 = rand() % 6 + 1;
+		roll2 = rand() % 6 + 1;		//Second dice roll
 		cout << roll2 << endl;
 		
-		sum = roll1 + roll2;
-		arr[sum-1]++;
+		sum = roll1 + roll2;	//Add them
+		arr[sum-2]++;		//to the array
 		cout << sum << " " << endl;
 		total += sum;
 	}
+	
 	cout << "Total value: "<<total << endl;
-	int count = 0;
+	double count = 0;
 	for(int i = 0; i < 11; i++){
 		count = arr[i];
 		//cout << i+1 <<": "<< ((count/12) *100) << "%"<<endl;
-		cout << i+2 << " rolled: " << arr[i] << endl;
+		cout << i+2 << " rolled: " << ((count/n)*100) << "%" << endl;
 	}
 
 return 0;
