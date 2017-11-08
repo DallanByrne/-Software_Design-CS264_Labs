79829376a786da2b8a41707e7e53347feabc34a9
Dallan Byrne <simplisticdude@gmail.com>
2017-11-08 22:08:28 +0000

    My work from home
    Upload adding additional functions

--- lab4/MyComplex.h
+++ lab4/MyComplex.h
@@ -19,6 +19,17 @@ class MyComplex(){
 		complex();
 		
 		complex(double, double);
-};
+		
+		double getReal()
+		void setReal(double)
+		double getImage()
+		void setImage(double)
+		
+		MyComplex Add(const MyComplex &z)
+		MyComplex Subtract(const MyComplex &z)
+		MyComplex Multiply(const MyComplex &z)
+
+		
+	};
 
 #endif
